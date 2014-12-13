508ffd1c0af4f2a70a94e68bbc3e7e6d58e976b5
DeanSS <deansalmonssnelton@gmail.com>
2014-12-13 20:59:24 +1100

    Admin Can Create Choices Directly For Questions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,11 +4,24 @@ from django.contrib import admin
 from django.contrib import admin
 from polls.models import Question, Choice
 
+#display choices
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
+    #display the question fields in a particular order
+    #with date collapsed initally
     fieldsets = [
         (None,               {'fields':['question_text']}),
         ('Date information', {'fields':['pub_date'], 'classes': ['collapse']}),
     ]
+    #display an option to create choices when creating questions
+    inlines = [ChoiceInline]
+
+    #change the way quesions are listed in the admin screen
+    list_display = ('question_text','pub_date', 'was_published_recently' )
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
