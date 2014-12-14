81639a727645bd8769e6f57e39c6134e429befe8
DeanSS <deansalmonssnelton@gmail.com>
2014-12-14 11:07:47 +1100

    Index Page Now Displays Latest Questions. /results /vote created

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,5 +5,16 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
+    #index view loading
+    #ex: polls/
     url(r'^$', views.index, name='index'),
+
+    #other view loading with pram
+    #ex: polls/
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    #ex: polls/5/results
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    #ex: polls/5/vote
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+
 )
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 
-
+#index view
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-# Create your views here.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
+
+#more views, but with arguments
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
