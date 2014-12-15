cd8a3f223a581cbf6049b9acfe505396868d2e08
DeanSS <deansalmonssnelton@gmail.com>
2014-12-15 13:11:52 +1100

    fixed Bug Revealed By Implemented Self Test

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,20 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
+
+from polls.models import Question
+
+#test that was_created_recently doean't pass questions made in the future
+class QuestionMethodTests(TestCase):
+
+    def test_was_created_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertEqual(future_question.was_published_recently(), False)
 
 # Create your tests here.
